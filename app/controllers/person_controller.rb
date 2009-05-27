114763add4343cd3d3aab7a8d2aa3ca780d3475f
David Medinets <davd.medinets@gmail.com>
2009-05-27 19:56:08 -0400

    restrict list columns

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -2,5 +2,6 @@ class PersonController < ApplicationController
   layout 'default'
   active_scaffold :person do |config|
     config.columns[:books].form_ui = :select
+    config.list.columns = [ :name, :books ]
   end
 end
