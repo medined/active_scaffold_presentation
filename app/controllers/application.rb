90c4f8207f47697ce56c1deae71b604445547129
David Medinets <davd.medinets@gmail.com>
2009-07-07 08:41:04 -0400

    turn off layouts for ajax requests.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  layout proc{ |c| c.request.xhr? ? false : "application" }
   #helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
